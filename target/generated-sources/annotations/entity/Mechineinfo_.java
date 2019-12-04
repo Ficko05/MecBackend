266dda318b8790fa2e02e6a98707e6fdc54fdc3e
Ficko <filip-f@hotmail.com>
2019-12-04 12:35:59 +0100

    date time push til steven

--- target/generated-sources/annotations/entity/Mechineinfo_.java
+++ target/generated-sources/annotations/entity/Mechineinfo_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-03T10:39:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-04T11:00:44")
 @StaticMetamodel(Mechineinfo.class)
 public class Mechineinfo_ { 
 
